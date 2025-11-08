613483d8d46ddbbbcf0c0e167e3522e8376fba8a
Steven Li <lishuyustevenli@163.com>
2025-11-07 19:11:05 -0500

    Refactor isCharging logic in Entity class and update it in main

--- exercises/08-shaders/CS3113/Entity.h
+++ exercises/08-shaders/CS3113/Entity.h
@@ -118,7 +118,6 @@ public:
     Texture2D   getCurrentTexture()        const { return mCurrentTexture;        }
     TextureType getTextureType()           const { return mTextureType;           }
     Animation   getAnimation()             const { return mAnimation;             }
-    int         isCharging()               const { return mAnimation == CHARGING ? 1 : 0; }
     int         getFrameSpeed()            const { return mFrameSpeed;            }
     float       getJumpingPower()          const { return mJumpingPower;          }
     bool        isJumping()                const { return mIsJumping;             }
